73ad3c6f95a40e3ad7069adb366a0c594a654376
Jaeyoung <jyko1101@gmail.com>
2024-09-30 12:04:51 +0900

    add: client manager

---
+++ Jykoserver/Protocols/ClientManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Concurrent;
+
+namespace Jykoserver.Protocols
+{
+    public class ClientManager
+    {
+        // Singleton으로 존재
+        private static readonly Lazy<ClientManager> _instance = new Lazy<ClientManager>(() => new ClientManager());
+        private ClientManager() { }
+        public static ClientManager Instance => _instance.Value;
+        // 클라이언트 목록
+        private ConcurrentDictionary<Guid, ClientInfo> clients = new ConcurrentDictionary<Guid, ClientInfo>();
+
+        public void AddClient(Guid guid, ClientInfo clientInfo)
+        {
+            clients.TryAdd(guid, clientInfo);
+        }
+        public void RemoveClient(Guid guid)
+        {
+            clients.TryRemove(guid, out var clientInfo);
+        }
+        public void Broadcast(byte[] msg)
+        {
+            foreach (var client in clients.Values)
+            {
+                //client.send
+            }
+        }
+    }
+
+    public class ClientInfo
+    {
+
+    }
+}
--- Jykoserver/obj/Debug/net8.0/Jykoserver.AssemblyInfo.cs
+++ Jykoserver/obj/Debug/net8.0/Jykoserver.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jykoserver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b99d399ba7ae3f127b6fee2d9dc4bc411b4225")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ad0552b185e009a0d88eef766ee63e40a33b46")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jykoserver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jykoserver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
